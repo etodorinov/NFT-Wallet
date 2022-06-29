7c05837beac799535b2feef606376c5d919853ea
etodorinov <todorinov@gmail.com>
2022-06-29 11:28:23 +0300

    Add login and register page

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,5 @@ const businessController = require("./controllers/businessController");
 router.use("/", homeController);
 router.use("/user", userController);
 router.use("/business", businessController);
-router.get("*", (req, res) => {
-  res.render("404");
-});
 
 module.exports = router;
